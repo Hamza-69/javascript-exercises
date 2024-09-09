da910304ce23b5dc30293ce0c283442bd6d02e80
Hamza El Rachdi <hamzarachidi04@gmail.com>
2024-09-10 00:42:04 +0300

    Solve problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringReturned = "";
+    if (num < 0) return "ERROR";
+    for (let i = 0; i<num;i++) {
+        stringReturned += string;
+    }
+    return stringReturned;
 };
 
 // Do not edit below this line
