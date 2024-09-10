43641dbb2ec9ed0ac3d81e050be8519c0f383f67
Hamza El Rachdi <hamzarachidi04@gmail.com>
2024-09-11 00:02:43 +0300

    Solve problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let returned = "";
+    for (let i =1; i <= string.length; i++) {
+        returned = returned + string.at(string.length - i)
+    }
+    return returned
 };
 
 // Do not edit below this line
