d72db9fe5b7225a7744ed34e0f68f18a92080396
Hamza El Rachdi <hamzarachidi04@gmail.com>
2024-09-11 02:07:45 +0300

    Solved 4th problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (item of args) {
+        if (array.includes(item)) {
+            array = array.filter(x => x!=item)
+        }
+    }
+    return array
 };
-
+console.log(removeFromArray([1,2,1,1], 1))
 // Do not edit below this line
 module.exports = removeFromArray;
