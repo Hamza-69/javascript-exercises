b85d8ce855b49829555718b0d0ebe034aa36734e
Hamza El Rachdi <hamzarachidi04@gmail.com>
2024-09-11 22:31:05 +0300

    Solve 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(lower, upper) {
+    if ((typeof lower) == 'number'  && (typeof upper ) == 'number') {
 
+    if (lower % 1 != 0 || upper % 1 != 0) return "ERROR";
+    if (lower < 0 || upper < 0 ) return "ERROR";
+    let n = upper > lower ? upper -lower +1: lower -upper+1;
+    return n*(n+1)/2 
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
